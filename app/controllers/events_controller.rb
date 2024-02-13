80f4afabbd074673f081c3ff4bb6c2ef534de3b1
bottles10 <henryaovare@gmail.com>
2024-02-13 16:21:37 -0600

    Add event edit and destroy method and view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,11 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
   def index
     @events = Event.all
   end
 
   def show
-    @event = Event.find(params[:id])
   end
 
   def new
@@ -22,7 +22,28 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to event_path(@event), status: :see_other
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @event.destroy!
+      
+      redirect_to events_path, status: :see_other
+    end
+
   private
+
+  def find_params
+    @event = Event.find(params[:id])
+  end
 
     def event_params
       params.require(:event).permit(:title, :location, :description, :date)
