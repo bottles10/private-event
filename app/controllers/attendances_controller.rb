f9c5cf6854474c0afa2af85e109eea7513c8ad1e
bottles10 <henryaovare@gmail.com>
2024-02-13 03:37:42 -0600

    Add attend event new method  and  view

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,21 @@
+class AttendancesController < ApplicationController
+  def new
+    @attendance = Attendance.new
+  end
+
+  def create
+    @attendance = current_user.attendances.build(attendance_params)
+    
+    if @attendance.save
+      redirect_to root_path, staus: :see_other
+      flash[:success] = "Successfully added Attendance!."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def attendance_params
+      params.require(:attendance).permit(:event_id)
+    end
+end
