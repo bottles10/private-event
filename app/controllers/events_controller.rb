1751c7989a562ba6ae04603fbc04073c89ced400
bottles10 <henryaovare@gmail.com>
2024-02-13 02:06:49 -0600

    Add event CRUD view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,30 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    
+    if @event.save
+      redirect_to event_path(@event), status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:title, :location, :description, :date)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
+  resources :users, only: [:show]
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
